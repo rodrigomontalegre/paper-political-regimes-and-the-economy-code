680dd2278369af6e57c5e73c863d99999576f2a1
Rodrigo MontAlegre <rodrigo.montalegre@hotmail.com>
2021-01-10 13:21:44 +0100

    System user identification.

--- paper_code.R
+++ paper_code.R
@@ -1,1 +1,6 @@
 #Code for the final paper handed in the semianr POL61405 "Political Regimes and the Economy"
+#Author: Rodrigo Mont'Alegre
+
+if (Sys.info()["user"] == "rodrigo") {
+  setwd("C:/Users/Rodrigo/Desktop/TUM/Wintersemester 2021/POL61405 Political Regimes and the Economy/Seminar Paper/Datasets")
+}
